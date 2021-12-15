66f2b9fb06376640b65ce7604ecbfe42385d3749
webdev03 <75148774+webdev03@users.noreply.github.com>
2021-12-15 19:04:16 +1300

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<div class="container">
+  <br />
+  <h1 style="text-align: center;"> Waka Kiwi Quiz </h1>
+  <p style="text-align: center;">Hello, Waka Family! Test out your Kiwi Quiz knowledge using this fun web
+      application. Ready to go?</p> <br />
+  <h2 style="text-align: center;">Instructions</h2>
+  <p style="text-align: center;">The higher the level, the harder the questions.</p>
+  <br />
+  <h2>Quizzes</h2>
+
+  <a class="btn btn-primary btn-lg" href="quizzes/level1/choose.html" role="button">Level 1 </a> <br /> <br />
+  <a class="btn btn-primary btn-lg" href="quizzes/level2/choose.html">Level 2</a> <br /> <br />
+  <footer><br />Made by Devarsh and Jasper.<br /><br /></footer>
+</div>
